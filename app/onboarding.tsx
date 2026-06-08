ce0bb2b7e81ae934074f9de485b97986a786f148
startdz <fuaddzakwan26@gmail.com>
2026-06-09 00:43:19 +0700

    navigation

--- app/onboarding.tsx
+++ app/onboarding.tsx
@@ -1,11 +1,14 @@
+import { styled } from "nativewind";
 import React from "react";
-import { Text, View } from "react-native";
+import { Text } from "react-native";
+import { SafeAreaView as RNSaveAreaView } from "react-native-safe-area-context";
+const SafeAreaView = styled(RNSaveAreaView);
 
 const onboarding = () => {
   return (
-    <View>
+    <SafeAreaView className="flex-1 items-center justify-center bg-background">
       <Text>onboarding</Text>
-    </View>
+    </SafeAreaView>
   );
 };
 
--- app/subscriptions/[id].tsx
+++ app/subscriptions/[id].tsx
@@ -1,14 +1,17 @@
 import { Link, useLocalSearchParams } from "expo-router";
+import { styled } from "nativewind";
 import React from "react";
-import { Text, View } from "react-native";
+import { Text } from "react-native";
+import { SafeAreaView as RNSaveAreaView } from "react-native-safe-area-context";
+const SafeAreaView = styled(RNSaveAreaView);
 
 const SubscriptionDetails = () => {
   const { id } = useLocalSearchParams<{ id: string }>();
   return (
-    <View>
+    <SafeAreaView className="flex-1 items-center justify-center bg-background">
       <Text>Subscriptions Detail: {id}</Text>
       <Link href="/">Go back</Link>
-    </View>
+    </SafeAreaView>
   );
 };
 
